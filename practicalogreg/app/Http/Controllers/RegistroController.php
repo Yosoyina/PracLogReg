f6f63330dcfbf3966680283439ec652a605d003c
yosoyina <inacio08123@gmail.com>
2026-01-19 23:07:01 +0100

    Reutilizndo el codigo del anterior repositorio
    Importamos auth para poder iniciar sesion, con la funcion creaste  lo utilizamos para mostar el formulario de registro a la hora de registrar el usuario  se guarda en la variable user lo que perimite iniciar sesion automaticamente y despues se redirige al usuario a su tabla despues de registrarse

--- practicalogreg/app/Http/Controllers/RegistroController.php
+++ practicalogreg/app/Http/Controllers/RegistroController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class RegistroController extends Controller
 {
-    //
+
+    public function create()
+    {
+        return view('auth.registro');
+    }
+
+    //Validar los datos del formulario
+
+    public function registro(Request $request){
+
+        $user = User::create([
+            'username' => $request->required('username'),
+            'name' => $request->required('name'),
+            'apellidos' => $request->required('apellidos'),
+            'email' => $request->required('email'),
+            'password' => Hash::make($request->required('password')),
+        ]);
+
+        Auth::login($user);
+        return redirect()->route('dashboard');
+    }
+
 }
