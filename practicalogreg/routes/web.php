63f3a688219de8c9627e70dd7ad3b6e5874586c7
yosoyina <inacio08123@gmail.com>
2026-01-20 00:23:31 +0100

    Configuracion de la rutas

--- practicalogreg/routes/web.php
+++ practicalogreg/routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegistroController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Muestra el formulario de registro
+Route::get('/registro', [RegistroController::class, 'create'])->name('registro');
+Route::post('/registro', [RegistroController::class, 'registro']);
+
+// Muestra el formulario de inicio de sesión
+Route::get('/login', [LoginController::class, 'create'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+
+Route::middleware([\App\Http\Middleware\Autentificacion::class])->group(function () {
+    // Muestra el dashboard según el rol del usuario (admin o user)
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
 });
