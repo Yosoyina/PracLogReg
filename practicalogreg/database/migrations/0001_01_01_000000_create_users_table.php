11ae9a41c882411399b5e5741aefac092413544f
yosoyina <inacio08123@gmail.com>
2026-01-20 00:29:15 +0100

    Anadir campos en el modelo User y en la migracion

--- practicalogreg/database/migrations/0001_01_01_000000_create_users_table.php
+++ practicalogreg/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('username')->unique();
             $table->string('name');
+            $table->string('apellidos');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('rol', ['admin', 'user'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
