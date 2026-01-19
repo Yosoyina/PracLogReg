856dee5d31a859cbd317045c9491ef13e598b85e
yosoyina <inacio08123@gmail.com>
2026-01-19 23:02:07 +0100

    Verificacion de Ususario
    Esto lo que hace es ver si el usuario no ha iniciado sesion

--- practicalogreg/app/Http/Middleware/Autentificacion.php
+++ practicalogreg/app/Http/Middleware/Autentificacion.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class Autentificacion
 {
@@ -15,6 +16,12 @@ class Autentificacion
      */
     public function handle(Request $request, Closure $next): Response
     {
+
+        // Verificar si el usuario está autenticado
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+
         return $next($request);
     }
 }
